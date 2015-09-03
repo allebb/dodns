0f91c0befe914f5bc7852511878f6727b680fb9a
Bobby Allen <ballen@bobbyallen.me>
2015-09-03 12:24:42 +0100

    Updated the example file with a new simple layout, a place for the user to set their own DO token and some code comments.

--- examples/test.php
+++ examples/test.php
@@ -1,5 +1,25 @@
 <?php
 require_once '../vendor/autoload.php';
+use Ballen\Dodns\CredentialManager;
+use Ballen\Dodns\Dodns;
+use Ballen\Dodns\Entities\DomainEntity;
 
-$test = new \Ballen\Dodns\Dodns(new \Ballen\Dodns\CredentialManager('fabcad1bb522f4b382be858d3f419c206e7b6e72248018bab5e71af8d448c593'));
-$test->domain(new \Ballen\Dodns\Entities\DomainEntity(['name' => 'alln.uk', 'ttl' => 3000, 'zone_file' => null]));
+// Set your DigitalOcean API key here!
+$digitalocean_api_v2_token = 'fabcad1bb522f4b382be858d3f419c206e7b6e72248018bab5e71af8d448c593';
+
+
+$test = new Dodns(new CredentialManager($digitalocean_api_v2_token));
+
+/**
+ * Example of getting and displaying all of your currently configured domains:
+ */
+$test->domains();
+
+/**
+ * Example of returning a single domain.
+ */
+$test->domain(new DomainEntity([
+    'name' => 'alln.uk',
+    'ttl' => 3000,
+    'zone_file' => null
+]));
