776cce41693d121868965d683ef05c7d8b1af573
Bobby Allen <ballen@bobbyallen.me>
2015-09-08 15:43:32 +0100

    Refactored the data validation implementation.

--- lib/Support/RecordBuilder.php
+++ lib/Support/RecordBuilder.php
@@ -3,44 +3,76 @@
 class RecordBuilder implements BuilderInterface
 {
 
-    private $build_data = [
+    const TYPE_STRING = 'string';
+    const TYPE_NULLABLE = 'null';
+    const TYPE_NUMBER = 'number';
+
+    private $object_data = [
         'type' => false,
         'name' => false,
         'data' => false,
-        'priority' => false,
-        'port' => false,
-        'weight' => false,
-    ];
-    private $valid_types = [
-        'A',
-        'AAAA',
-        'CNAME',
-        'TXT',
-        'SRV',
-    ];
-    private $required_data = [
-        'A',
-        'AAAA',
-        'CNAME',
-        'MX',
-        'TXT',
-        'SRV',
-        'NS',
-    ];
-    private $required_priority = [
-        'MX',
-        'SRV',
-    ];
-    private $required_port = [
-        'SRV',
-    ];
-    private $required_weight = [
-        'SRV',
+        'priority' => null,
+        'port' => null,
+        'weight' => null,
     ];
 
-    public function type()
+    /**
+     * Validation rules (data types/allowed values for each field)
+     * @var array
+     */
+    private $validation = [
+        'type' => [
+            'A',
+            'AAAA',
+            'CNAME',
+            'TXT',
+            'SRV',
+        ],
+        'name' => self::TYPE_STRING,
+        'data' => self::TYPE_STRING,
+        'priority' => [
+            self::TYPE_NULLABLE,
+            self::TYPE_NUMBER,
+        ],
+        'port' => [
+            self::TYPE_NULLABLE,
+            self::TYPE_NUMBER,
+        ],
+        'weight' => [
+            self::TYPE_NULLABLE,
+            self::TYPE_NUMBER,
+        ]
+    ];
+
+    /**
+     * Set of required field values dependent on the 'type' field value.
+     * @var array
+     */
+    private $required = [
+        'data' => ['A',
+            'AAAA',
+            'CNAME',
+            'MX',
+            'TXT',
+            'SRV',
+            'NS',
+        ],
+        'priority' => [
+            'MX',
+            'SRV',
+        ],
+        'port' => [
+            'SRV',
+        ],
+        'weight' => [
+            'SRV',
+        ],
+    ];
+
+    public function type($data)
     {
-        
+        $this->validateDataTypes($data, $type);
+        $this->object_data[$type] = $data;
     }
 
     public function name()
@@ -70,6 +102,16 @@ class RecordBuilder implements BuilderInterface
 
     public function requestBody()
     {
-        ;
+        
+    }
+
+    private function validateRequired($data, $field)
+    {
+        
+    }
+
+    private function validateDataType($data, $field)
+    {
+        
     }
 }
